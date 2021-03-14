0f370f81165abfb5a9be85f886b00f4b06396f2d
comic. dev <simplyanimatedvideos@gmai.com>
2021-03-14 14:18:08 +0100

    fix(ignore): Ignore dist folder

--- dist/src/Config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Prefix = void 0;
-const config_json_1 = require("../config.json");
-async function Prefix(msg) {
-    return config_json_1.prefix;
-}
-exports.Prefix = Prefix;
-//# sourceMappingURL=Config.js.map
