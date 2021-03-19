49c0633de7702808813d981d18d772db8566bda4
comic. dev <simplyanimatedvideos@gmai.com>
2021-03-19 08:36:01 +0100

    fix(yarn):

---
+++ lib/extensions/Command.ts
@@ -0,0 +1,24 @@
+import {
+	Command as AkairoCommand,
+	CommandOptions as AkairoCommandOptions
+} from 'discord-akairo';
+export class Command extends AkairoCommand {
+	public constructor(id: string, options?: CommandOptions) {
+		super(id, {
+			...options,
+			channel: options.channel ?? 'guild'
+		});
+	}
+
+	public description: CommandDescription;
+}
+
+interface CommandOptions extends AkairoCommandOptions {
+	description: CommandDescription;
+}
+
+interface CommandDescription {
+	content: string;
+	usage: string;
+	examples: string[];
+}
---
+++ src/Commands/Mod Commands/BanCommand.ts
@@ -0,0 +1,103 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { Command } from '../../../lib/extensions/Command';
+import { Util } from '../../../lib/extensions/Util';
+import MS from 'ms';
+import { GuildMember } from 'discord.js';
+export default class BanCommand extends Command {
+	public constructor() {
+		super('ban', {
+			aliases: ['ban'],
+			category: 'Moderation',
+			description: {
+				content: 'Bans the specified member',
+				usage: '$ban [ member ] ( time ) [ reason ]',
+				examples: [
+					'$ban comic 1d Breaking the rules',
+					'$ban @comic.#6949 Simping'
+				]
+			},
+			cooldown: 3000,
+			userPermissions: ['BAN_MEMBERS'],
+			clientPermissions: ['BAN_MEMBERS'],
+			args: [
+				{
+					id: 'member',
+					type: 'member',
+					prompt: {
+						start: `Please specify an member to ban`,
+						retry: `Please specify an valid member to ban`
+					}
+				},
+				{
+					id: 'time',
+					type: (message, phrase) => {
+						if (phrase && MS(phrase) > 0) return MS(phrase);
+						if (['perm', 'permanent'].includes(phrase.toLowerCase()))
+							return 'Permanent';
+						return null;
+					},
+					prompt: {
+						start: `Please specify an time period`,
+						retry: `Please specify an valid time period`
+					}
+				},
+				{
+					id: 'reason',
+					type: 'string',
+					default: 'No reason specified'
+				}
+			]
+		});
+	}
+
+	public async exec(
+		message: Message,
+		{
+			member,
+			time,
+			reason
+		}: {
+			member: GuildMember;
+			time: any;
+			reason: string;
+		}
+	): Promise<any> {
+		if (member.id === message.member.id)
+			return message.channel
+				.send(new MessageEmbed().setDescription('You cannot ban yourself.'))
+				.then((m) =>
+					setTimeout(() => {
+						m.delete();
+					}, 5000)
+				);
+		if (
+			(member.roles.highest.position > message.member.roles.highest.position &&
+				message.guild.owner.id !== message.member.id) ||
+			member.id === message.guild.owner.id
+		)
+			return message.channel
+				.send(
+					new MessageEmbed().setDescription(
+						`**${member.user.username}** has an higher staff position than you. You cannot ban them`
+					)
+				)
+				.then((m) =>
+					setTimeout(() => {
+						m.delete();
+					}, 5000)
+				);
+		member
+			.send(
+				new MessageEmbed().setDescription(
+					`You've been banned from **${message.guild.name}** with the reason of **${reason}**`
+				)
+			)
+			.catch(() => null);
+		member.ban({ reason: reason });
+		if (time.toLowerCase() !== 'permanent') {
+			setTimeout(() => {
+				message.guild.members.unban(member.id, 'Ban period expired');
+			}, time);
+		} else return;
+	}
+}
