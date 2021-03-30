c1f646c77e81d8c83cd6bf0330fe97ea28913909
comic. dev <simplyanimatedvideos@gmai.com>
2021-03-30 11:17:43 +0200

    feat(presence): Add client presence

--- src/Events/ReadyListener.ts
+++ src/Events/ReadyListener.ts
@@ -10,5 +10,9 @@ export default class ReadyListener extends Listener {
 
 	public exec(): void {
 		console.log(`[${this.client.user.username}] Connected to Discord Gateway`);
+		this.client.user.setActivity({
+			type: 'WATCHING',
+			name: `@Atomic help | ${this.client.users.cache.size.toLocaleString()} users`
+		});
 	}
 }
